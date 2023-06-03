ca22bfe6a481feb04339e9fdc08b576ef0d1fefc
hari230403 <135420641+hari230403@users.noreply.github.com>
2023-06-03 15:41:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function append(value) {
+    document.getElementById("result").value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function deleteLast() {
+    var result = document.getElementById("result").value;
+    document.getElementById("result").value = result.slice(0, -1);
+  }
+  
+  function calculate() {
+    var result = document.getElementById("result").value;
+    try {
+      var answer = eval(result);
+      document.getElementById("result").value = answer;
+    } catch (error) {
+      document.getElementById("result").value = "Error";
+    }
+  }
+  
